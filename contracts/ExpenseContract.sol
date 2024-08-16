fbbfd045804a45b8b7b8d9aa1a743f4c2fbd28e5
aarav0180 <aarav.18o2005@gmail.com>
2024-08-17 00:52:51 +0530

    smart contract modified

--- contracts/ExpenseContract.sol
+++ contracts/ExpenseContract.sol
@@ -17,25 +17,44 @@ contract ExpenseContract{
         owner = msg.sender;
     }
 
+    mapping( address => uint) public balances;
+
     event Deposit(address indexed _from, uint _amount, string _reason, uint _timestamp);
     event Withdraw(address indexed _from, uint _amount, string _reason, uint _timestamp);
 
     function deposit(uint _amount, string memory _reason) public payable {
         require(_amount> 0, "Deposit amount can't be less than 0");
+        balances[msg.sender] += _amount;
         transactions.push(Transaction(msg.sender, _amount, _reason, block.timestamp));
         emit Deposit(msg.sender, _amount, _reason, block.timestamp);
 
     }
 
-    function withdraw (uint _amount, string memory _reason) public payable {}
+    function withdraw (uint _amount, string memory _reason) public {
+        require(balances[msg.sender] >= _amount, "Insufficient balance");
+        balances[msg.sender] -= _amount;
+        transactions.push(Transaction(msg.sender, _amount, _reason, block.timestamp));
+        payable(msg.sender).transfer(_amount);
+        emit(msg.sender, _amount, _reason, block.timestamp);
+    }
 
-    function getBalance(){}
+    function getBalance(address _account)public view returns(uint){
+        return balances[_account];
+}
 
-    function getTransCount(){}
+    function getTransCount()public view returns(uint){
+        return transactions.length;
+    }
 
-    function getTotalTrans(){}
+    function getTotalTrans()public view returns(address[] memory, uint[] memory, string[] memory, uint[] memory){
 
-    function getTrans(){}
+    }
+
+    function getTrans(uint _index)public view returns(address, uint, string memory, uint){
+        require(_index < transactions.length, "Index out of bounds");
+        Transaction memory transaction = transactions[_index];
+        return (transaction.user, transaction.amount, transaction.reason, transaction.timestamp);
+    }
 
     function changeOwner(){}
 }
