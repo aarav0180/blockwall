549be1e7dccc40524d9be59b97ff349e03dc6228
aarav0180 <aarav.18o2005@gmail.com>
2024-08-22 01:34:31 +0530

    new ui and transaction model

---
+++ lib/model/transaction_model.dart
@@ -0,0 +1,9 @@
+class TransactionModel {
+  final String address;
+  final int amount;
+  final String reason;
+  final DateTime timestamp;
+
+  TransactionModel(this.address, this.amount, this.reason, this.timestamp);
+
+}
